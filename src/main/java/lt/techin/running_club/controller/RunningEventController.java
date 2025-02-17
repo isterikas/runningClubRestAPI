c85529db2ad7c3f6b93bfe1b6136d390c84694e5
Eryk Tatol <eryk.tatol@stud.techin.lt>
2025-02-17 10:41:58 +0200

    deleting events functionality added.

--- src/main/java/lt/techin/running_club/controller/RunningEventController.java
+++ src/main/java/lt/techin/running_club/controller/RunningEventController.java
@@ -3,15 +3,14 @@ package lt.techin.running_club.controller;
 import jakarta.validation.Valid;
 import lt.techin.running_club.dto.RunningEventMapper;
 import lt.techin.running_club.dto.RunningEventRequestDTO;
+import lt.techin.running_club.dto.RunningEventResponseDTO;
 import lt.techin.running_club.dto.UserMapper;
 import lt.techin.running_club.model.RunningEvent;
 import lt.techin.running_club.service.RunningEventService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
@@ -26,7 +25,7 @@ public class RunningEventController {
   }
 
   @PostMapping("/events")
-  public ResponseEntity<?> createEvent(@Valid @RequestBody RunningEventRequestDTO runningEventRequestDTO) {
+  public ResponseEntity<RunningEventResponseDTO> createEvent(@Valid @RequestBody RunningEventRequestDTO runningEventRequestDTO) {
     RunningEvent runningEvent = new RunningEvent();
     runningEvent.setName(runningEventRequestDTO.name());
     runningEvent.setCalendarDate(runningEventRequestDTO.calendarDate());
@@ -37,5 +36,14 @@ public class RunningEventController {
             .path("/{id}")
             .buildAndExpand(runningEvent.getId())
             .toUri()).body(RunningEventMapper.toRunningEventResponseDTO(runningEvent));
+  }
+
+  @DeleteMapping("/events/{eventId}")
+  public ResponseEntity<Void> deleteEvent(@PathVariable long eventId) {
+    if (!runningEventService.existsById(eventId)) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+    runningEventService.deleteById(eventId);
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
   }
 }
--- src/main/java/lt/techin/running_club/service/RunningEventService.java
+++ src/main/java/lt/techin/running_club/service/RunningEventService.java
@@ -17,4 +17,12 @@ public class RunningEventService {
   public RunningEvent save(RunningEvent runningEvent) {
     return runningEventRepository.save(runningEvent);
   }
+
+  public boolean existsById(long eventId) {
+    return runningEventRepository.existsById(eventId);
+  }
+
+  public void deleteById(long eventId) {
+    runningEventRepository.deleteById(eventId);
+  }
 }
